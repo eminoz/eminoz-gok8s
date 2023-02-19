536078713c57064823e4c1aa2121a42272900964
eminoz <eminoz@yahoo.com>
2023-02-19 18:10:59 +0300

    proje establisted

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON("hello from go??")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+	app.Listen(":" + port)
+}
